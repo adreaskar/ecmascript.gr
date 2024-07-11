05a8accb0f51e99214bbc104f7683d2bfc24707b
adreaskar <adrian26mccous@gmail.com>
2024-07-11 12:31:21 +0300

    Refactored home page

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,64 +1,68 @@
-import clsx from 'clsx';
-import Heading from '@theme/Heading';
-import styles from './styles.module.css';
+import { Pin, Globe, Rss } from 'lucide-react';
 
 const FeatureList = [
-  {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
-  },
-  {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
-  },
+    {
+        title: 'Ξεκινήστε απο εδώ',
+        Icon: <Pin strokeWidth={2.25} />,
+        description: (
+            <>
+                Η εισαγωγή στην γλώσσα με παραδείγματα και οδηγίες για την
+                καλύτερη κατανόηση και εκμάθηση της.
+            </>
+        ),
+        link: '/docs-language/intro',
+    },
+    {
+        title: 'Επόμενο βήμα, ο Browser',
+        Icon: <Globe strokeWidth={2.25} />,
+        description: (
+            <>
+                Μάθετε πως να χρησιμοποιείτε τις λειτουργικότητες του Βrowser
+                στην JavaScript, όπως το DOM, τα events και τα cookies.
+            </>
+        ),
+        link: '/docs-browser/intro',
+    },
+    {
+        title: 'Περισσότερα κεφάλαια σύντομα',
+        Icon: <Rss strokeWidth={2.25} />,
+        description: (
+            <>
+                Περισσότερα κεφάλαια και θέματα έρχονται σύντομα. Μείνετε
+                συντονισμένοι!
+            </>
+        ),
+        link: '#',
+    },
 ];
 
-function Feature({Svg, title, description}) {
-  return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <Heading as="h3">{title}</Heading>
-        <p>{description}</p>
-      </div>
-    </div>
-  );
+function Feature({ Icon, title, description, link }) {
+    return (
+        <a
+            href={link}
+            className="w-full flex-1 p-5 bg-[var(--ifm-color-grey)] rounded-lg text-[var(--ifm-hero-text-color)] hover:no-underline"
+        >
+            <div className="text-left text-[var(--ifm-color-primary-light)] mb-2">
+                {Icon}
+            </div>
+            <div className="text-left">
+                <h3>{title}</h3>
+                <p className="mb-0">{description}</p>
+            </div>
+        </a>
+    );
 }
 
 export default function HomepageFeatures() {
-  return (
-    <section className={styles.features}>
-      <div className="container">
-        <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
-        </div>
-      </div>
-    </section>
-  );
+    return (
+        <section className="items-center py-20 px-0 w-full">
+            <div className="container">
+                <div className="row gap-10">
+                    {FeatureList.map((props, idx) => (
+                        <Feature key={idx} {...props} />
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,39 +1,6 @@
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import useBaseUrl from '@docusaurus/useBaseUrl';
-import ThemedImage from '@theme/ThemedImage';
-
-import Heading from '@theme/Heading';
-
-function HomepageHeader() {
-    const { siteConfig } = useDocusaurusContext();
-    return (
-        <header className="hero text-center relative overflow-hidden p-8 lg:py-16">
-            <div className="container">
-                <Heading
-                    as="h1"
-                    className="hero__title text-4xl font-semibold lg:text-6xl"
-                >
-                    {siteConfig.title}
-                </Heading>
-                <p className="hero__subtitle mt-2 text-base lg:text-lg">
-                    {siteConfig.tagline}
-                </p>
-                <div className="flex items-center justify-center mt-10">
-                    <ThemedImage
-                        alt="Greek developer image"
-                        sources={{
-                            light: useBaseUrl('/img/ecma-logo.png'),
-                            dark: useBaseUrl('/img/ecma-logo-dark.png'),
-                        }}
-                        className="w-[200px]"
-                    />
-                </div>
-            </div>
-        </header>
-    );
-}
+import HomepageHeader from '@site/src/components/HomepageHeader';
 
 export default function Home() {
     return (
@@ -42,6 +9,8 @@ export default function Home() {
             description="The best Greek JavaScript guide around!"
         >
             <HomepageHeader />
+
+            <div className="fade-stripe h-[7px]"></div>
 
             <main>
                 <HomepageFeatures />
