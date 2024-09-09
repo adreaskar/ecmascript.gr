eea567d1dd971e698d5734850dffda6c931188f2
adreaskar <adrian26mccous@gmail.com>
2024-09-09 17:51:55 +0300

    Updated footer

--- src/theme/Footer/Layout/index.js
+++ src/theme/Footer/Layout/index.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import clsx from 'clsx';
-export default function FooterLayout({style, links, logo, copyright}) {
-  return (
-    <footer
-      className={clsx('footer', {
-        'footer--dark': style === 'dark',
-      })}>
-      <div className="container container-fluid">
-        {links}
-        {(logo || copyright) && (
-          <div className="footer__bottom text--center">
-            {logo && <div className="margin-bottom--sm">{logo}</div>}
-            {copyright}
-          </div>
-        )}
-      </div>
-    </footer>
-  );
+export default function FooterLayout({ style, links, logo, copyright }) {
+    return (
+        <footer className="footer">
+            <div className="container container-fluid">
+                {links}
+                {(logo || copyright) && (
+                    <div className="footer__bottom text--center">
+                        {logo && (
+                            <div className="margin-bottom--sm">{logo}</div>
+                        )}
+                        {copyright}
+                    </div>
+                )}
+            </div>
+        </footer>
+    );
 }
